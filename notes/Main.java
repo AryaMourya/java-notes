106313c35a93fecb95b7a14ba7d479cbd6a1bb62
AryaMourya <aryamourya0@gmail.com>
2026-06-14 13:47:33 +0530

    commit and save all changes

--- notes/Main.java
+++ notes/Main.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) { 
        Scanner input = new Scanner(System.in);
-       System.out.print(input.nextInt());
+       System.out.print(input.nextLine());
     }
 }
---
+++ notes/primitives.java
@@ -0,0 +1,10 @@
+package notes;
+
+public class primitives {
+    public static void main(String[] args){
+        int calling = 64;
+
+        // String is not a primitive data type, it is a class
+        String name = "Aadya";
+    }
+}
