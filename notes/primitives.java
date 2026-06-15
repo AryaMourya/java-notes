11b08003542e1e3077a05a66731b9ff88e91369f
AryaMourya <aryamourya0@gmail.com>
2026-06-15 20:08:07 +0530

    commit and save all changes

--- notes/primitives.java
+++ notes/primitives.java
@@ -3,7 +3,11 @@ package notes;
 public class primitives {
     public static void main(String[] args){
         int calling = 64;
-
+        char grade = 'A';
+        boolean isCorrect = true;
+        float percentage = 99.9f;
+        double largeDecimal = 123456789.123456789;
+        long largeInteger = 1234567890123456789L;
         // String is not a primitive data type, it is a class
         String name = "Aadya";
     }
