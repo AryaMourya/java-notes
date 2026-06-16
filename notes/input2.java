8c53a005e975a711d8217c1b4ec7f29670274711
AryaMourya <aryamourya0@gmail.com>
2026-06-16 09:37:39 +0530

    commit and save all changes

---
+++ notes/input2.java
@@ -0,0 +1,15 @@
+package notes;
+
+import java.util.Scanner;
+
+public class input2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println(name);
+
+        float num = input.nextFloat();
+        System.out.println(num);
+
+    }
+}
