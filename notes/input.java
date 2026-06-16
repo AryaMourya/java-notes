6799fc6e59ff89321d26aa139775dbbef8b04bdb
AryaMourya <aryamourya0@gmail.com>
2026-06-16 09:14:19 +0530

    commit and save all changes

---
+++ notes/input.java
@@ -0,0 +1,13 @@
+package notes;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your roll number: ");
+        int rollno = input.nextInt();
+        System.out.println("Your roll number is: " + rollno);
+    }
+    
+}
