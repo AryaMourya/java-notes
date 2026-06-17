057909b0b3afef71eddb14521a7e82d39ea51ddc
AryaMourya <aryamourya0@gmail.com>
2026-06-17 12:02:08 +0530

    commit and save all changes

---
+++ notes/lec2/loops.java
@@ -0,0 +1,45 @@
+package notes.lec2;
+
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+        /*
+           syntax of for loop in Java
+
+
+           for(initialization; condition; increment/decrement) {
+               // code to be executed
+           }
+        */
+       // Q: Print numbers from 1 to 5
+         for(int num = 1; num <= 5; num += 2){
+            System.out.println(num);         
+         }
+        
+      // Q: Print numbers from 1 to n WITH HELLO WORLD
+      Scanner in = new Scanner(System.in);
+      int n = in.nextInt();
+      for(int num = 1; num<= n ; num++){
+          System.out.print(num +" ");
+          System.out.println("Hello World");
+      }
+
+    
+     // WHILE LOOP
+     /*
+        syntax of while loop in Java
+
+        while(condition) {
+            // code to be executed
+        }
+     */
+
+        // Q: Print numbers from 1 to 5
+        int num = 1;
+        while(num <= 5){
+            System.out.print(num + " ");
+            num++;
+        }
+}
+}
