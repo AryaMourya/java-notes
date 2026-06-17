0bba3042c851400a3ec133349c2f2d238429abb1
AryaMourya <aryamourya0@gmail.com>
2026-06-17 08:38:49 +0530

    commit and save all changes

--- notes/lec2/conditions.java
+++ notes/lec2/conditions.java
@@ -2,6 +2,33 @@ package notes.lec2;
 
 public class conditions {
     public static void main(String[] args){
-        
-    }
+        /*
+           Syntax of if statement in Java
+           if(condition in boolean expression  T/F){
+               // code to be executed if condition is true
+           } else {
+               // code to be executed if condition is false
+           }
+        */
+       int salary = 25000; 
+       if (salary < 20000) {
+           salary = salary + 5000;
+       } else {
+           salary = salary + 2000;
+       }
+       System.out.println("New salary: " + salary);
+
+       // Multiple if-else statements
+       if (salary > 10000) {
+        salary += 2000;
+       } else if (salary > 20000) {
+        salary += 3000;
+       } else if (salary > 30000) {
+        salary += 4000;
+       } else {
+        salary += 5000;
+       }
+       System.out.println("New salary after multiple conditions: " + salary);
+
+    }   
 }
