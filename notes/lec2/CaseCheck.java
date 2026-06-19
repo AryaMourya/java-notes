bd2f450a66ad16b704ce02f2771ce3fe116d2416
AryaMourya <aryamourya0@gmail.com>
2026-06-19 08:25:54 +0530

    commit and save all changes

--- notes/lec2/CaseCheck.java
+++ notes/lec2/CaseCheck.java
@@ -6,5 +6,16 @@ public class CaseCheck {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         char ch = in.next().trim().charAt(0);
+        System.out.println(ch);
+
+        if (ch >= 'a' && ch <= 'z'){
+            System.out.println("Lowercase");
+        }
+        else if (ch >= 'A' && ch <= 'Z'){
+            System.out.println("Uppercase");
+        }
+        else{
+            System.out.println("Not an alphabet");
+        }
     }
 }
