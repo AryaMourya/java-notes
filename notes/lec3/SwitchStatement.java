9dd4b95fea28c2ddc1d4812b410d4a5b20375cda
AryaMourya <aryamourya0@gmail.com>
2026-06-21 10:40:31 +0530

    commit and save all changes

--- notes/lec3/SwitchStatement.java
+++ notes/lec3/SwitchStatement.java
@@ -7,5 +7,19 @@ public class SwitchStatement {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String fruit = in.next();       
-}
+
+        switch(fruit){
+            case "apple":
+                System.out.println("Apple is red");
+                break;
+            case "banana":
+                System.out.println("Banana is yellow");
+                break;
+            case "grape":
+                System.out.println("Grape is purple");
+                break;
+            default:
+                System.out.println("Unknown fruit");
+        }
+    }
 }
