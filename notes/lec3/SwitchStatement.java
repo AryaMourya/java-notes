f4af2d8a9e2b4976ddb0861e66b885fc3c218678
AryaMourya <aryamourya0@gmail.com>
2026-06-21 10:19:53 +0530

    commit and save all changes

---
+++ notes/lec3/SwitchStatement.java
@@ -0,0 +1,11 @@
+package notes.lec3;
+
+import java.util.Scanner;
+
+public class SwitchStatement {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String fruit = in.next();       
+}
+}
