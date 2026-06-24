0d7b1e0a48e9463e4ff557991833379d536c6d8b
AryaMourya <aryamourya0@gmail.com>
2026-06-24 12:57:21 +0530

    commit and save all changes

---
+++ notes/lec5/Array1.java
@@ -0,0 +1,12 @@
+package notes.lec5;
+
+public class Array1 {
+    //syntax
+    //datatype[] arrayName = new datatype[size];    
+    //store 5 roll numbers
+    int[] rollNumbers = new int[5];
+    //or 
+    int[] rollNumbers2 = {101, 102, 103, 104, 105};
+
+    int[] ros; // declaration of array. ros is getting defined in the stack
+}
