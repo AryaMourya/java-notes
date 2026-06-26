42a6e7b498b92a5758129efa3d3da047343c1bb5
AryaMourya <aryamourya0@gmail.com>
2026-06-26 11:18:59 +0530

    commit and save all changes in it

--- notes/lec5/Array1.java
+++ notes/lec5/Array1.java
@@ -8,5 +8,17 @@ public class Array1 {
     //or 
     int[] rollNumbers2 = {101, 102, 103, 104, 105};
 
-    int[] ros; // declaration of array. ros is getting defined in the stack
+    public static void main(String[] args) {
+        int[] ros; // declaration of array. ros is getting defined in the stack
+        ros = new int[5]; // actually here is being created in the memory (heap)
+
+        int[] arr = new int[5];
+        arr[0] = 10;    
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+        //[10, 20, 30, 40, 50]
+        System.out.println(arr[0]); // 10
+    }
 }
---
+++ notes/lec5/stringarray.java
@@ -0,0 +1,9 @@
+package notes.lec5;
+
+public class stringarray {
+    public static void main(String[] args) {
+      String[] names = new String[5];
+      System.out.println(names[0]); // null
+        }
+    }
+
