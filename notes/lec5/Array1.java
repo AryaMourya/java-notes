f78dca318f7fda273ead205a9dd18c50558fe35d
AryaMourya <aryamourya0@gmail.com>
2026-06-29 08:53:15 +0530

    commit and save all changes

--- notes/lec5/Array1.java
+++ notes/lec5/Array1.java
@@ -1,5 +1,8 @@
 package notes.lec5;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Array1 {
     //syntax
     //datatype[] arrayName = new datatype[size];    
@@ -9,6 +12,7 @@ public class Array1 {
     int[] rollNumbers2 = {101, 102, 103, 104, 105};
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         int[] ros; // declaration of array. ros is getting defined in the stack
         ros = new int[5]; // actually here is being created in the memory (heap)
 
@@ -20,5 +24,20 @@ public class Array1 {
         arr[4] = 50;
         //[10, 20, 30, 40, 50]
         System.out.println(arr[0]); // 10
+
+        //input using for loop
+        for(int i =0; i< arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+
+        for (int i =0; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        for(int num : arr){ //
+            System.out.print(num + " ");
+        }
+        /* toString()Method of Arrays class is used to convert the array into string representation.
+         */
+        System.out.println(Arrays.toString(arr));
     }
 }
