fc188739837a85d512b8314b00dd52c1b2a67e3b
AryaMourya <aryamourya0@gmail.com>
2026-06-29 21:31:38 +0530

    commit and save all changes

---
+++ notes/lec5/MultiDimension.java
@@ -0,0 +1,12 @@
+package notes.lec5;
+
+public class MultiDimension {
+    public static void main(String[] args){  
+        /*
+         1 2 3
+         4 5 6
+         7 8 9
+        */
+       
+      }
+}
--- notes/lec5/toStringMethod.java
+++ notes/lec5/toStringMethod.java
@@ -11,5 +11,11 @@ public class toStringMethod {
             arr[i] = in.nextInt();
         }
         System.out.println(Arrays.toString(arr));
-}
+        // Array of objects
+        String[] str = new String[4];
+        for(int i=0; i<str.length; i++){
+            str[i] = in.next();
+        }
+        System.out.println(Arrays.toString(str));
+    }
 }
