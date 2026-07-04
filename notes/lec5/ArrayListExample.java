cf935753b2951431797e81d5eb4c4f0ef6433495
AryaMourya <aryamourya0@gmail.com>
2026-07-04 23:46:26 +0530

    commit and save all changes

--- notes/lec5/ArrayListExample.java
+++ notes/lec5/ArrayListExample.java
@@ -5,7 +5,16 @@ import java.util.ArrayList;
 public class ArrayListExample {
     public static void main(String[] args){
         // syntax
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<Integer>(10);
+
+        list.add(67);
+        list.add(45);
+        list.add(23);
+        list.add(89);
+        list.add(12);
+        list.add(34);
+
+        System.out.println(list);
     }
 }
 
