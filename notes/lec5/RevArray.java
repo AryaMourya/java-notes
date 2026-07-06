1aeafc44f4ece49e7fb801957bbcb9f0cc45096b
AryaMourya <aryamourya0@gmail.com>
2026-07-06 13:17:02 +0530

    commit and save all changes

--- notes/lec5/RevArray.java
+++ notes/lec5/RevArray.java
@@ -1,5 +1,23 @@
 package notes.lec5;
 
 public class RevArray {
-    
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5};
+        reverse(arr);
+        System.out.print(java.util.Arrays.toString(arr));
+        
+
+    }
+    static int[] reverse(int arr[]){
+        int start = 0;
+        int end = arr.length-1;
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return arr;
+    }
 }
