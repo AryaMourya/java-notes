6bbd865a0e62d5e4e007858aa4061087a19f7443
AryaMourya <aryamourya0@gmail.com>
2026-07-13 15:14:16 +0530

    commit and save all changes

---
+++ notes/Searching/SearchIn2Darray.java
@@ -0,0 +1,25 @@
+package notes.Searching;
+public class SearchIn2Darray {
+    public static void main(String[] args){
+        int[][] arr = {
+            {23,4,1},
+            {56,78,90},
+            {12,34,67},
+            {10, 20, 30}
+        };
+
+        int target = 34;
+        int[] ans = search(arr, target);
+        System.out.println(java.util.Arrays.toString(ans));
+    }
+    static int[] search(int arr[][], int target){
+        for (int row = 0; row < arr.length;row++){
+            for (int col = 0; col < arr[row].length; col++){
+                if (arr[row][col]== target){
+                    return new int[]{row, col};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
