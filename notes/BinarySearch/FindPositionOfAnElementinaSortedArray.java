04c70144b7f8dbf16fc6ae95138c5da7b4e1c8d3
AryaMourya <aryamourya0@gmail.com>
2026-07-22 08:42:58 +0530

    commit and save this question type with solution

---
+++ notes/BinarySearch/FindPositionOfAnElementinaSortedArray.java
@@ -0,0 +1,41 @@
+package notes.BinarySearch;
+
+public class FindPositionOfAnElementinaSortedArray {
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 5;
+        int result = ans(arr, target);
+        System.out.println("Position: " + result);
+    }
+    static int ans(int [] arr, int target){
+        //first find the range
+        // first start with a box of size 2 
+        int start = 0;
+        int end = 1;
+
+        // condition for the target to lie in the range 
+        while (target > arr[end]){
+            int newStart = end +1;
+            //double the box value
+            // end = prev end + sizeof box * 2
+            end = end + (end - start + 1) * 2 ;
+            start = newStart;
+        }
+        return binarySearch(arr, target, start, end);
+    }
+    static int binarySearch(int[] arr, int target, int start , int end){
+        while(start<= end){
+            // find the middle element 
+            int mid = start + (end - start )/2;
+
+            if (target < arr[mid]){
+                end = mid -1 ;
+            }else if (target > arr[mid]){
+                start = mid +1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
